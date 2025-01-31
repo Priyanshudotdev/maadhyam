287a9ebe7c713680fab405a539f298bd8b638b32
Priyanshudotdev <priyanshu.kayarkar009@gmai.com>
2025-01-31 21:03:01 +0530

    remove the red color

--- src/Pages/Read.jsx
+++ src/Pages/Read.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Read = () => {
   return (
     <div className="w-full pt-[6rem] min-h-screen flex flex-col items-center py-8 px-6">
-      <div className=" bg-red-600 w-full h-screen ">
+      <div className="w-full h-screen">
         <iframe
           className="w-full h-full"
           src="https://online.fliphtml5.com/pczxe/kszu/"
